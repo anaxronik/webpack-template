73e4f25cb02b6e3d5d457bc69996d5ca8ccbf3a6
Sergey Komarov <anaxronik@gmail.com>
2021-05-18 16:23:40 +0500

    fix file trash

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -12,7 +12,7 @@ module.exports = {
   },
   output: {
     filename: '[name].[contenthash].js',
-    path: path.resolve(__dirname, 'dist'), // = ./dist
+    path: path.resolve(__dirname, 'docs'), // = ./dist
     clean: true,
   },
   plugins: [
